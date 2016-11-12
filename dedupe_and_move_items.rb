8d33fe35c4a17f09ca63d68dd6efcf6723bf454c
Sam Eckmeier <eckmeier41@gmail.com>
2016-11-12 16:13:51 -0500

    removes Items filter code and replaces with filtering from Podio api, and refactors Podio::Client

--- dedupe_and_move_items.rb
+++ dedupe_and_move_items.rb
@@ -6,18 +6,21 @@ require 'uri'
 AUTH_KEYS = [:username, :password, :client_id, :client_token]
 ITEM_FIELD_TO_DEDUPE = 'title'
 ITEM_FILTER = { 'Scheduling Status' => 'Date confirmed' }
+SCHEDULING_STATUS_FIELD_ID = 133224490
+SCHEDULING_STATUS_VALUE_FILTER = [3]
 
 def run(options)
   podio_client = Podio::Client.new(options)
   podio_items = Podio::Items.new(podio_client)
 
   puts "Requesting items\n"
-  json_items = podio_items.find_all(options[:app_id])
-  puts "Successfully requested items\n\n"
+  filters = { 'filters' => { SCHEDULING_STATUS_FIELD_ID => SCHEDULING_STATUS_VALUE_FILTER }}
+  items = podio_items.find_all(options[:app_id], filters)
+  puts "Successfully requested #{items.count} items\n\n"
 
-  items = JSON.parse(json_items)['items']
+  puts "Deduping #{items.count} items"
   items = Podio::Items.dedupe(items, ITEM_FIELD_TO_DEDUPE)
-  items = Podio::Items.filter_by_field(items, ITEM_FILTER)
+  puts "Successful dedupe -- items count: #{items.count}\n\n"
 
   puts "Posting items"
   items.each_with_idex do |item, index|
@@ -39,31 +42,36 @@ module Podio
       @client_id = args[:client_id]
       @client_secret = args[:client_secret]
       @oauth_token = authenticate({
-        username: args[:username],
-        password: args[:password],
-        client_id: args[:client_id],
-        client_secret: args[:client_secret]
+        'username' => args[:username],
+        'password' => args[:password],
+        'client_id' => args[:client_id],
+        'client_secret' => args[:client_secret],
+        'grant_type' => 'password'
       })
     end
 
-    def authenticate(args)
+    def authenticate(params)
       token = nil
 
-      args[:grant_type] = 'password'
-      auth_slug = "oauth/token"
-      res = post(auth_slug, args)
+      headers = default_headers({
+        content_type: 'application/x-www-form-urlencoded',
+        accept: 'application/x-www-form-urlencoded'
+      })
 
+      encoded_params = URI.encode_www_form(params)
+      res = post("#{@url}/oauth/token", encoded_params, headers)
       oauth_hsh = JSON.parse(res)
       token = oauth_hsh["access_token"]
 
       token
     end
 
-    def get(slug)
+    def get(slug, headers=nil)
       payload = nil
 
-      uri = add_oauth("#{@url}/#{slug}")
-      res = Net::HTTP.get_response(uri)
+      uri = uri(slug)
+      request = request(uri)
+      res = request.get(uri, headers || headers())
 
       handle_http_code(res, "#{res.code} ERROR: Failed GET request for #{slug}")
 
@@ -71,11 +79,13 @@ module Podio
       payload
     end
 
-    def post(slug, body)
+    def post(slug, params, headers=nil)
       payload = nil
 
-      uri = add_oauth("#{@url}/#{slug}")
-      res = Net::HTTP.post_form(uri, body)
+      uri = uri(slug)
+      request = request(uri)
+
+      res = request.post(uri.path, params, headers || headers())
 
       handle_http_code(res, "#{res.code} ERROR: Failed POST request for #{slug} --- #{res.body}")
 
@@ -85,13 +95,33 @@ module Podio
 
     private
 
-    def add_oauth(uri)
-      formatted_uri = URI(uri)
+    def uri(slug)
+      URI("#{@url}/#{slug}")
+    end
 
-      params = { oauth_token: @oauth_token }
-      formatted_uri.query = URI.encode_www_form(params)
+    def headers(args={})
+      headers = default_headers(args)
+      headers = headers.merge(authentication_headers()) if @oauth_token
 
-      formatted_uri
+      headers
+    end
+
+    def default_headers(args={})
+      {
+        'Content-Type' => args[:content_type] || 'application/json',
+        'Accept'=> args[:accept] || 'application/json'
+      }
+    end
+
+    def authentication_headers()
+      { 'Authorization' => "OAuth2 #{@oauth_token}" }
+    end
+
+    def request(uri)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      http
     end
 
     def handle_http_code(response, error_msg)
@@ -107,55 +137,27 @@ module Podio
       @client = client
     end
 
-    def find_all(app_id)
-      @client.get("item/app/#{app_id}/")
+    def find_all(app_id, filters=nil)
+      items = nil
+      base_slug = "/item/app/#{app_id}"
+
+      if filters
+        encoded_filters = JSON.generate(filters)
+        items = @client.post("#{base_slug}/filter/", encoded_filters)
+      else
+        items = @client.get("#{base_slug}/")
+      end
+
+      items = JSON.parse(items)['items']
+      items
     end
 
-    def post(app_id, body)
+    def post(app_id, items)
       @client.post("item/app/#{app_id}/", body)
     end
 
     def self.dedupe(items, field)
-      puts "Deduping #{items.count} items"
-      deduped_items = items.uniq { |obj| obj[field] }
-      puts "Successful dedupe -- items count: #{deduped_items.count}\n\n"
-
-      deduped_items
-    end
-
-    def self.filter_by_field(items, filter)
-      filtered = []
-
-      items.each do |item|
-        match = true
-
-        fields_to_check = map_fields_to_check(filter, item)
-
-        filter.each { |k,v| match = false unless fields_to_check[k] === v }
-        filtered << item if match
-      end
-
-      filtered
-    end
-
-    private
-
-    def self.map_fields_to_check(filter, item)
-      fields_to_check = {}
-      field_objs = item['fields']
-
-      field_objs.each do |field_obj|
-        key = field_obj['label']
-        val = field_obj['values'][0]
-
-        if val.class == Hash && val['value'].class == Hash
-          val = val['value']['text']
-        end
-
-        fields_to_check[key] = val if filter.keys.include?(key)
-      end
-
-      fields_to_check
+      items.uniq { |obj| obj[field] }
     end
   end
 end
