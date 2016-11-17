8710faf9d455f7a89fa0ea986c6cd5fc184a76d5
Sam Eckmeier <eckmeier41@gmail.com>
2016-11-17 10:04:03 -0500

    adds Root component

---
+++ d3_app/src/components/Root/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header';
+import Chart from '../Chart';
+import './Root.scss';
+
+const Root = () => {
+  return (
+    <div className="Root">
+      <Header />
+      <Chart />
+    </div>
+  );
+};
+
+export default Root;
