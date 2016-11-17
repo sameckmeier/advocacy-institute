0e5d4471dd22610bc3897f3d99ed01d7dc5f0349
Sam Eckmeier <eckmeier41@gmail.com>
2016-11-17 10:05:03 -0500

    adds Chart component

--- d3_app/src/components/Chart/Chart.js
+++ d3_app/src/components/Chart/Chart.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
-import './App.css';
+import BarGraph from '../BarGraph';
+import PodioApi, { fieldsByType, d3Format } from '../../utilities/podio';
+import './Chart.scss';
 
-class App extends Component {
+export default class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      displayedDataIndex: 0,
+    };
+  }
+
+  componentDidMount() {
+    PodioApi.meetingsAppData((error, response, body) => {
+      const req_data = JSON.parse(body);
+      const fields = fieldsByType(req_data.items, 'category');
+      const data = d3Format(fields);
+
+      this.setState({ data })
+    });
+  }
+
+  toggleData(index) {
+    this.setState({
+      displayedDataIndex: index,
+    })
+  }
+
   render() {
+    const { displayedDataIndex, data } = this.state
+    const fieldNames = data.map(obj => obj.fieldName);
+    const vertices = data[displayedDataIndex] ? data[displayedDataIndex].vertices : [];
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="Chart">
+        <BarGraph vertices={vertices}/>
+        <div className="Chart-data-toggles">
+          {
+            fieldNames.map((fieldName, i) => {
+              return (
+                <button
+                  key={i}
+                  className='Chart-data-toggle'
+                  onClick={this.toggleData.bind(this, i)}
+                >
+                  {fieldName}
+                </button>
+              );
+            })
+          }
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
-
-export default App;
